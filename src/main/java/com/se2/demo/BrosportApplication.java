778705e5b7694f4e74d5b28433e285ed2dd7be2c
ducsieunhan <Chroller03@gmail.com>
2026-01-28 22:40:59 +0700

    init

---
+++ src/main/java/com/se2/demo/BrosportApplication.java
@@ -0,0 +1,13 @@
+package com.se2.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrosportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrosportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/se2/demo/BrosportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.se2.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrosportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
