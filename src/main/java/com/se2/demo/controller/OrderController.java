1b298b8d83ae7ed3f24e60767c0019fda4f065bd
VuongMinhVu <vmvhlls123@gmail.com>
2026-03-23 22:20:28 +0700

    Order update logic(except VNPAY)

--- src/main/java/com/se2/demo/controller/OrderController.java
+++ src/main/java/com/se2/demo/controller/OrderController.java
@@ -1,30 +1,45 @@
 package com.se2.demo.controller;
 
 import com.se2.demo.dto.request.OrderRequest;
+import com.se2.demo.dto.response.OrderDetailResponse;
+import com.se2.demo.dto.response.OrderHistoryResponse;
 import com.se2.demo.dto.response.OrderResponse;
 import com.se2.demo.service.OrderService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/orders")
 @RequiredArgsConstructor
 public class OrderController {
-    @Autowired
+
     private final OrderService orderService;
 
     @PostMapping("/checkout")
-    public ResponseEntity<String> checkout(@RequestBody OrderRequest request, HttpServletRequest servletRequest) {
-        String paymentUrl = orderService.preparePayment(request, servletRequest);
-        return ResponseEntity.ok(paymentUrl);
+    public ResponseEntity<OrderResponse> checkout(@RequestBody OrderRequest request, HttpServletRequest httpServletRequest) {
+        OrderResponse response = orderService.checkout(request, httpServletRequest);
+        return ResponseEntity.ok(response);
     }
 
     @GetMapping("/vnpay-callback")
     public ResponseEntity<OrderResponse> handleCallback(HttpServletRequest request) {
         OrderResponse response = orderService.processPaymentCallback(request);
         return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/history/{userId}")
+    public ResponseEntity<List<OrderHistoryResponse>> getOrderHistory(@PathVariable Integer userId) {
+        List<OrderHistoryResponse> history = orderService.getOrderHistory(userId);
+        return ResponseEntity.ok(history);
+    }
+
+    @GetMapping("/{orderCode}")
+    public ResponseEntity<OrderDetailResponse> getOrderDetail(@PathVariable String orderCode) {
+        OrderDetailResponse detail = orderService.getOrderDetail(orderCode);
+        return ResponseEntity.ok(detail);
     }
 }
